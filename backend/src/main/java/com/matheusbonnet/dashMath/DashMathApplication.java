3951dec4a35a8cf56e3771027d9e489ecb273585
Matheus Bonnet <matheusbonnet2019@gmail.com>
2021-09-07 10:56:30 -0300

    Projetos criados

---
+++ backend/src/main/java/com/matheusbonnet/dashMath/DashMathApplication.java
@@ -0,0 +1,13 @@
+package com.matheusbonnet.dashMath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashMathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashMathApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/matheusbonnet/dashMath/DashMathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusbonnet.dashMath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashMathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
